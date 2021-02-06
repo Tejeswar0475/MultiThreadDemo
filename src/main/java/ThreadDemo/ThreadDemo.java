95fd5119ce8c30dfd00502f7e6708fea875b5c60
Tejeswar0475 <tejeswar096@gmail.com>
2021-02-06 15:54:25 +0530

    Added:MultiThread demo

---
+++ src/main/java/ThreadDemo/ThreadDemo.java
@@ -0,0 +1,28 @@
+package ThreadDemo;
+
+class MyThread extends Thread
+{
+	public void run()
+	{
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("Child Thread");
+		}
+	}
+	
+}
+
+public class ThreadDemo {
+
+	public static void main(String[] args) {
+		Thread t=new Thread();
+		t.start();
+		for(int i=0;i<=10;i++)
+		{
+			System.out.println("Hello:"+i);
+			
+		} 
+		
+	}
+
+}
